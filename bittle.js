e822c6316278e6127997ffdea5e97ed4975ad6f2
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2024-09-21 18:32:58 -0400

    Update bittle.js

--- bittle.js
+++ bittle.js
@@ -1,4 +1,5 @@
 const SerialPort = require('serialport').SerialPort;
+/*
 const comPortB = new SerialPort({
 	path: '/dev/cu.usbmodem56D00039821',
 	baudRate: 115200,
@@ -13,3 +14,22 @@ comPortB.on('open', function () {
 		console.log('>:^.^:>\n' + data);
 	});
 });
+*/
+class UART {
+	constructor(port, baudRate, dataBits, stopBits, parity) {
+		this.comPortB = new SerialPort(port, {
+			baudRate: baudRate,
+			dataBits: dataBits,
+			stopBits: stopBits,
+			parity: parity,
+		});
+
+		this.comPortB.on('open', function () {
+			console.log('Serial Port connected');
+			this.comPortB.on('data', function (data) {
+				console.log('>:^.^:>\n' + data);
+			});
+		});
+		
+	}
+}
