798dee2d60555b2baa15cd7d55fe04ae11b5ed03
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2024-09-21 19:58:57 -0400

    Constructor
    +Constructor finished with successful connection

--- bittle.js
+++ bittle.js
@@ -1,35 +1,22 @@
 const SerialPort = require('serialport').SerialPort;
-/*
-const comPortB = new SerialPort({
-	path: '/dev/cu.usbmodem56D00039821',
-	baudRate: 115200,
-	dataBits: 8,
-	stopBits: 1,
-	parity: 'none',
-});
 
-comPortB.on('open', function () {
-	console.log('Serial Port connected');
-	comPortB.on('data', function (data) {
-		console.log('>:^.^:>\n' + data);
-	});
-});
-*/
 class UART {
-	constructor(port, baudRate, dataBits, stopBits, parity) {
-		this.comPortB = new SerialPort(port, {
+	constructor(path, baudRate, dataBits, stopBits, parity) {
+		this.comPortB = new SerialPort({
+			path: path,
 			baudRate: baudRate,
 			dataBits: dataBits,
 			stopBits: stopBits,
 			parity: parity,
 		});
 
-		this.comPortB.on('open', function () {
+		this.comPortB.on('open', () => {
 			console.log('Serial Port connected');
-			this.comPortB.on('data', function (data) {
+			this.comPortB.on('data', (data) => {
 				console.log('>:^.^:>\n' + data);
 			});
 		});
-		
 	}
 }
+
+const comPortB = new UART('/dev/cu.usbmodem56D00039821', 115200, 8, 1, 'none');
